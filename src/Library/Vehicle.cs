64df40bb2332dbe2d97d47e9a77309ba280377bc
matoferreira <matifer1@gmail.com>
2021-05-13 20:23:09 -0300

    Creada clase vehicle

---
+++ src/Library/Vehicle.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Library
+{
+    public class Vehicle
+    {
+        private int vIN;
+        private String brand;
+        private String model;
+        private int year;
+        private String licensePlate;
+
+        
+    }
+}
