757206a6cbd72b74153f232f9320a107ba0238ab
matoferreira <matifer1@gmail.com>
2021-05-13 20:31:45 -0300

    Terminada clase vehicle

--- src/Library/Vehicle.cs
+++ src/Library/Vehicle.cs
@@ -9,7 +9,38 @@ namespace Library
         private String model;
         private int year;
         private String licensePlate;
-
-        
+        public int VIN;
+        public String Brand;
+        public String Model;
+        public int Year;
+        public String LicensePlate;
+        public Vehicle (int vIN, String brand, String model, int year, String licensePlate)
+        {
+            this.VIN = vIN;
+            this.Brand = brand;
+            this.Model = model;
+            this.Year = year;
+            this.LicensePlate = licensePlate;
+        }
+        public int GetVIN()
+        {
+            return VIN;
+        }
+        public String GetBrand()
+        {
+            return Brand;
+        }
+        public String GetModel()
+        {
+            return Model;
+        }
+        public int GetYear()
+        {
+            return Year;
+        }
+        public String GetLicensePlate()
+        {
+            return LicensePlate;
+        }
     }
 }
