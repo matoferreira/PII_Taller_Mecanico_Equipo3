3727170666b6e2e0963ee64aa0c0cb7ff7a7eff3
jandresleal <62621149+jandresleal@users.noreply.github.com>
2021-05-13 20:31:37 -0300

    creo order.cs

---
+++ src/Library/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Library
+{
+    public class Order
+    {
+        private int id;
+        public int Id;
+        private string date;
+        public string Date;
+        private string type;
+        public string Type;
+        private Vehicle assignedVehicle;
+        public Vehicle AssignedVehicle;
+        private ReplacementPartList partsUsed;
+        public ReplacementPartList PartsUsed;
+        private int supervisorWorkHours; 
+        public int SupervisorWorkHours; 
+        private int repairmanWorkHours; 
+        public int RepairmanWorkHours; 
+        private string status;
+        public string Status;
+        private Mechanic supervisor;
+        public Mechanic Supervisor;
+        private Mechanic repairman;
+        public Mechanic Repairman;
+
+    }
+}
