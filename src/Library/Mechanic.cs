9bbe4060d08165a23808422c3f8046972e9447ac
matoferreira <matifer1@gmail.com>
2021-05-18 18:03:14 -0300

    Correcciones en clase mechanic.cs

--- src/Library/Mechanic.cs
+++ src/Library/Mechanic.cs
@@ -9,18 +9,21 @@ namespace Library
         private int cI;
         private int hourlyRate;
         private string speciality;
+        private string role;
         public String Name;
         public String LastName;
         public int CI;
         public int HourlyRate;
         public string Speciality;
-        public Mechanic (String name, string lastName, int cI, int hourlyRate, String speciality)
+        public string Role;
+        public Mechanic (String name, string lastName, int cI, int hourlyRate, String speciality, String role)
         {
             this.Name = name;
             this.LastName = lastName;
             this.CI = cI;
             this.HourlyRate = hourlyRate;
             this.Speciality = speciality;
+            this.Role = role;
         }
     }
 }
