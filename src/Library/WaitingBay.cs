0f89cd48a89046fa00afef9bb93a36fb1d6f03f5
SoniaOlivera <61550104+SoniaOlivera@users.noreply.github.com>
2021-05-18 22:03:32 -0300

    cambio de ubicacion

---
+++ src/Library/WaitingBay.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+
+namespace Library
+{
+    public class WaitingBay
+    {
+        private ArrayList VehiclesWaiting(Vehicle);
+
+        private void AddVehicle (){
+            VehiclesWaiting.Add(Vehicle);
+        }
+
+        private void ExitVehicles (){
+            VehiclesWaiting.Remove(Vehicle);
+        }
+    }
+}
