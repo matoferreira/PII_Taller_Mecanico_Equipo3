0e3b9b797bcd45b45efd5a19808215a78e64e7e3
jandresleal <62621149+jandresleal@users.noreply.github.com>
2021-05-18 23:52:44 -0300

    cambios commit

--- src/Library/WorkingBay.cs
+++ src/Library/WorkingBay.cs
@@ -10,10 +10,12 @@ namespace Library
 
         private static int vehicleNumber = 0;
         private WaitingBay waitingBay = new WaitingBay();
-
-    
+        Mechanic mecanico2 = new Mechanic("Juan", "Leal", 43944249, 600, "Pintor", "Supervisor");
+        Mechanic mecanico1 = new Mechanic("Matias", "Ferreira", 46875005, 450, "chapista", "Repairman");
         public void AddVehicle (Vehicle auto){
             if (vehicleNumber<3){
+                Order orden = new Order("1/1/21", "Pintura", auto, mecanico1, mecanico2);
+                Order.NewOrder(orden);
                 VehiclesWorking.Add(auto);
                 vehicleNumber++;
                 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,23 +10,16 @@ namespace Program
         {
              
             WorkingBay workingBay = new WorkingBay();
-            Mechanic mecanico2 = new Mechanic("Juan", "Leal", 43944249, 600, "Pintor", "Supervisor");
-            Mechanic mecanico1 = new Mechanic("Matias", "Ferreira", 46875005, 450, "chapista", "Repairman");
             Vehicle auto = new Vehicle(123, "Mercedes", "Benz", 1999, "ABC1224");
             Vehicle auto2 = new Vehicle(123, "Mercedes", "Benz", 1999, "ABC1224");
             Vehicle auto3 = new Vehicle(123, "Mercedes", "Benz", 1999, "ABC1224");
             Vehicle auto4 = new Vehicle(123, "Mercedes", "Benz", 1999, "ABC1224");
             Vehicle auto5 = new Vehicle(123, "Mercedes", "Benz", 1999, "ABC1224");
-            Order orden = new Order("1/1/21", "Pintura", auto, mecanico1, mecanico2);
-            Order orden2 = new Order("1/1/21", "Pintura", auto, mecanico1, mecanico2);
-            Order orden3 = new Order("1/1/21", "Pintura", auto, mecanico1, mecanico2);
-            Order orden4 = new Order("1/1/21", "Pintura", auto, mecanico1, mecanico2);
-            Order orden5 = new Order("1/1/21", "Pintura", auto, mecanico1, mecanico2);
-            Order.NewOrder(orden);
-            Order.NewOrder(orden2);
-            Order.NewOrder(orden3);
-            Order.NewOrder(orden4);
-            Order.NewOrder(orden5);
+            workingBay.AddVehicle(auto);
+            workingBay.AddVehicle(auto2);
+            workingBay.AddVehicle(auto3);
+            workingBay.AddVehicle(auto4);
+            workingBay.AddVehicle(auto5);
         }
     }
 }
